88786137295609f16d36aea9934c4c2eb1cb1d19
ranjitsgill <82792839+ranjitsinghgill@users.noreply.github.com>
2022-11-30 09:24:28 +1100

    Updated both hero card and monster card

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 /*
-Challenge: 
-1. Take the hard-coded HTML for the Wizard card, bring it 
-   into index.js and then inject it back into its div with 
-   JavaScript.
-2. Do the same for Orc card. 
+CHALLENGE
+1. Strip out the hero and monster data (element id, name, avatar, 
+health and dice score) and store them in variables
+
+2. Write a renderCharacter() function that accepts the 5 new variables 
+as paramaters and renders out a character with this data
+
+3. Call renderCharacter() twice. Once with the hero variables and 
+once with the monster variables to that both are rendered
 */
 
-const hero = document.getElementById('hero')
+let elementIdHero = 'hero'
+let nameHero = 'Wizard'
+let avatarHero = 'images/wizard.png'
+let healthHero = 60
+let diceScoreHero = 6
 
-hero.innerHTML = 
-`           <div class="character-card">
-                <h4 class="name"> Wizard </h4>
-                <img class="avatar" src="images/wizard.png"/>
-                <p class="health">health: <b> 60 </b></p>
-                <div class="dice-container"><div class="dice"> 6 </div></div>
-            </div>`
+let elementIdMonster = 'monster'
+let nameMonster = 'Orc'
+let avatarMonster = "images/orc.png"
+let healthMonster = 10
+let diceScoreMonster = 4
+
+function renderCharacter(elementId, name, avatar, health, diceScore) {
+    document.getElementById(elementId).innerHTML = 
+    `<div class="character-card">
+        <h4 class="name"> ${name} </h4>
+        <img class="avatar" src="${avatar}"/>
+        <p class="health">health: <b> ${health} </b></p>
+        <div class="dice-container"><div class="dice"> ${diceScore} </div></div>
+    </div>`; 
+}
+
+renderCharacter(elementIdHero, nameHero, avatarHero, healthHero, diceScoreHero)
+renderCharacter(elementIdMonster, nameMonster, avatarMonster, healthMonster, diceScoreMonster)
