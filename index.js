767f4595e9d576c5db9ebda09d164ccc028dcaef
ranjitsgill <82792839+ranjitsinghgill@users.noreply.github.com>
2022-12-01 11:10:19 +1100

    Updating the index.js with destructing

--- index.js
+++ index.js
@@ -1,42 +1,37 @@
-/*
-CHALLENGE
-1. Convert our consts into two objects called 
-"monster" and "hero".
-2. Update the renderCharacter() function so that it accepts 
-a single object "data" as its parameter instead of five string/numbers, 
-reducing the number of arguments to pass in from five to one.
-3. Update the template now each variable is coming from "data".
-4. Update the function call.
-*/
-
 const hero = {
-   elementId: "hero",
-   name: "Wizard",
-   avatar: "images/wizard.png",
-   health: 60,
-   diceRoll: 6,
+    elementId: "hero",
+    name: "Wizard",
+    avatar: "images/wizard.png",
+    health: 60,
+    diceRoll: 3,
 }
 
 const monster = {
-   elementId: "monster",
-   name: "Orc",
-   avatar: "images/orc.png",
-   health: 10,
-   diceRoll: 4,
+    elementId: "monster",
+    name: "Orc",
+    avatar: "images/orc.png",
+    health: 10,
+    diceRoll: 4,
 }
+
+// CHALLENGE
+// 1. Deconstruct the data object 
+// 2. Update the template string as necessary
+
 
 
 function renderCharacter(data) {
-   document.getElementById(data.elementId).innerHTML =
-       `<div class="character-card">
-           <h4 class="name"> ${data.name} </h4>
-           <img class="avatar" src="${data.avatar}" />
-           <div class="health">health: <b> ${data.health} </b></div>
-           <div class="dice-container">
-               <div class="dice"> ${data.diceRoll} </div>
-           </div>
-       </div>`
+    let {elementId, name, avatar, health, diceRoll} = data
+    document.getElementById(elementId).innerHTML =
+        `<div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}" />
+            <div class="health">health: <b> ${health} </b></div>
+            <div class="dice-container">
+                <div class="dice"> ${diceRoll} </div>
+            </div>
+        </div>`
 }
 
-renderCharacter(hero);
-renderCharacter(monster);
+renderCharacter(hero)
+renderCharacter(monster)
