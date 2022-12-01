5059a97b5741d4abed98da1cf0449dbc6584bf25
ranjitsgill <82792839+ranjitsinghgill@users.noreply.github.com>
2022-12-01 13:24:28 +1100

    Updating diceRoll with for loop

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const hero = {
     avatar: "images/wizard.png",
     health: 60,
     diceRoll: 3,
+    diceCount: 3
 }
 
 const monster = {
@@ -12,26 +13,40 @@ const monster = {
     avatar: "images/orc.png",
     health: 10,
     diceRoll: 4,
+    diceCount: 1
 }
 
-// CHALLENGE
-// 1. Deconstruct the data object 
-// 2. Update the template string as necessary
-
-
-
 function renderCharacter(data) {
-    let {elementId, name, avatar, health, diceRoll} = data
-    document.getElementById(elementId).innerHTML =
+    const {elementId, name, avatar, health, diceRoll, diceCount } = data;
+    
+    let diceHtml = ''
+    for (let i = 0; i < diceCount; i++) {
+        diceHtml += `
+        <div class="dice">6</div>`
+    }
+    
+/*
+CHALLENGE
+1. declare a let called diceHtml and initialize it with an empty 
+string. 
+2. Use a for loop to update diceHtml so that it contains the 
+HTML for our dice. The number of dice needed is specificed in 
+the diceCount property of the objects.
+3. Each dice should have the following HTML: <div class="dice">6</div>
+4. For now, each dice will display 6
+5. Swap out the diceRoll variable for diceHtml in the template
+*/
+    
+    document.getElementById(elementId).innerHTML = 
         `<div class="character-card">
             <h4 class="name"> ${name} </h4>
             <img class="avatar" src="${avatar}" />
             <div class="health">health: <b> ${health} </b></div>
             <div class="dice-container">
-                <div class="dice"> ${diceRoll} </div>
+                ${diceHtml}
             </div>
-        </div>`
+        </div>`   
 }
 
-renderCharacter(hero)
-renderCharacter(monster)
+renderCharacter(hero);
+renderCharacter(monster);
