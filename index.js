cf05172ea76ab0663a971588f6261d8e376b400f
ranjitsgill <82792839+ranjitsinghgill@users.noreply.github.com>
2022-12-04 13:08:42 +1100

    Swapping out for loop for map in index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const hero = {
-    elementId: "hero", 
+    elementId: "hero",
     name: "Wizard",
     avatar: "images/wizard.png",
     health: 60,
@@ -12,34 +12,38 @@ const monster = {
     name: "Orc",
     avatar: "images/orc.png",
     health: 10,
-    diceRoll: [2],
+    diceRoll: [6],
     diceCount: 1
 }
 
+/*
+CHALLENGE
+1. Instead of the for loop, map over the diceRoll array
+and save the new array to diceHTML.
+2. Remember to deal with the commas between dice.
+3. What keyword should be used to declare diceHTML? 
+13
+*/
+
 function renderCharacter(data) {
-    const {elementId, name, avatar, health, diceRoll, diceCount } = data;
-    let diceHtml = '';
-
-// CHALLENGE
-// Update this for loop so it uses a value from the 
-// new diceRoll array to render out the dice so the 
-// wizard's dice have values of 3, 1 and 4, and the
-// orc's single dice has a value of 2.
-
-     
-for (let dice of data.diceRoll) {
-    diceHtml += `<div class="dice">${dice}</div>`
-}
+    const { elementId, name, avatar, health, diceRoll, diceCount } = data;
+    const diceHtml = diceRoll.map(function(roll){
+       return `<div class="dice">${roll}</div>`
+    });
     
-    document.getElementById(elementId).innerHTML = 
-    `<div class="character-card">
-        <h4 class="name"> ${name} </h4>
-        <img class="avatar" src="${avatar}" />
-        <div class="health">health: <b>${health}</b></div>
-        <div class="dice-container">
-            ${diceHtml}
-        </div>
-    </div>`; 
+    // for (let i = 0; i < diceRoll.length; i++) {
+    //     diceHtml += `<div class="dice">${diceRoll[i]}</div>`
+    // }
+
+    document.getElementById(elementId).innerHTML =
+        `<div class="character-card">
+            <h4 class="name"> ${name} </h4>
+            <img class="avatar" src="${avatar}" />
+            <div class="health">health: <b> ${health} </b></div>
+            <div class="dice-container">
+                ${diceHtml.join('')}
+            </div>
+        </div>`
 }
 
 renderCharacter(hero);
