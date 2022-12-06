80f7c5b365625ab4a40a21a3aaa1c82321674e54
ranjitsgill <82792839+ranjitsinghgill@users.noreply.github.com>
2022-12-06 16:08:25 +1100

    Updating constructor function to render the random numbers

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-function getDiceRollArray(diceCount) { 
-    return new Array(diceCount).fill(0).map(function(){
-    return Math.floor(Math.random() * 6) + 1
-    });   
+function getDiceRollArray(diceCount) {
+    return new Array(diceCount).fill(0).map(function () {
+        return Math.floor(Math.random() * 6) + 1
+    });
 }
 
 function getDiceHtml(diceCount) {
-    return getDiceRollArray(diceCount).map(function(num){ 
-        return  `<div class="dice">${num}</div>`
+    return getDiceRollArray(diceCount).map(function (num) {
+        return `<div class="dice">${num}</div>`
     }).join('')
 }
 
@@ -26,25 +26,23 @@ const monster = {
     diceCount: 1
 }
 
-function Character(data){
-    this.elementId = data.elementId
+/*
+7
+*/
+
+function Character(data) {
+    this.elementId = data.elementId;
     this.name = data.name;
     this.avatar = data.avatar;
     this.health = data.health;
     this.diceCount = data.diceCount;
-}
 
-/*
-6
-*/
+    this.getCharacterHtml = function () {
+        const { elementId, name, avatar, health, diceCount } = this;
+        const diceHtml = getDiceHtml(diceCount)
 
-
-function renderCharacter(data) {
-    const { elementId, name, avatar, health, diceCount } = data;
-    const diceHtml = getDiceHtml(diceCount)
-
-    document.getElementById(elementId).innerHTML =
-        `<div class="character-card">
+        document.getElementById(elementId).innerHTML =
+            `<div class="character-card">
             <h4 class="name"> ${name} </h4>
             <img class="avatar" src="${avatar}" />
             <div class="health">health: <b> ${health} </b></div>
@@ -52,7 +50,12 @@ function renderCharacter(data) {
                 ${diceHtml}
             </div>
         </div>`;
+
+    }
+
 }
 
-renderCharacter(hero);
-renderCharacter(monster);
+const wizard = new Character(hero)
+const orc = new Character(monster)
+wizard.getCharacterHtml()
+orc.getCharacterHtml()
