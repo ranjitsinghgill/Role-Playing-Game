edb4eb14b3eb8cd99bc4bc8085a06c6a31361535
ranjitsgill <82792839+ranjitsinghgill@users.noreply.github.com>
2022-12-07 15:29:19 +1100

    Simplifying constructor function with Object.assign

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-function getDiceRollArray(diceCount) {
-    return new Array(diceCount).fill(0).map(function () {
-        return Math.floor(Math.random() * 6) + 1
-    });
+function getDiceRollArray(diceCount) {  
+    return new Array(diceCount).fill(0).map(function(){
+    return Math.floor(Math.random() * 6) + 1
+    });   
 }
 
 function getDiceHtml(diceCount) {
-    return getDiceRollArray(diceCount).map(function (num) {
-        return `<div class="dice">${num}</div>`
+    return getDiceRollArray(diceCount).map(function(num){ 
+        return  `<div class="dice">${num}</div>`
     }).join('')
 }
 
@@ -26,36 +26,38 @@ const monster = {
     diceCount: 1
 }
 
-/*
-7
-*/
-
 function Character(data) {
-    this.elementId = data.elementId;
-    this.name = data.name;
-    this.avatar = data.avatar;
-    this.health = data.health;
-    this.diceCount = data.diceCount;
+/*
+9
+*/
+    // this.elementId = data.elementId
+    // this.name = data.name
+    // this.avatar = data.avatar
+    // this.health = data.health
+    // this.diceCount = data.diceCount
+    
+    Object.assign(this, data)
 
+    
     this.getCharacterHtml = function () {
-        const { elementId, name, avatar, health, diceCount } = this;
-        const diceHtml = getDiceHtml(diceCount)
-
-        document.getElementById(elementId).innerHTML =
-            `<div class="character-card">
-            <h4 class="name"> ${name} </h4>
-            <img class="avatar" src="${avatar}" />
-            <div class="health">health: <b> ${health} </b></div>
-            <div class="dice-container">    
-                ${diceHtml}
-            </div>
-        </div>`;
-
+        const { elementId, name, avatar, health, diceCount } = this;      
+        let diceHtml = getDiceHtml(diceCount);
+        
+        document.getElementById(elementId).innerHTML = `
+            <div class="character-card">
+                <h4 class="name"> ${name} </h4>
+                <img class="avatar" src="${avatar}" />
+                <div class="health">health: <b> ${health} </b></div>
+                <div class="dice-container">
+                    ${diceHtml}
+                </div>
+            </div>`;
     }
-
+    
 }
 
 const wizard = new Character(hero)
-const orc = new Character(monster)
 wizard.getCharacterHtml()
+
+const orc = new Character(monster)
 orc.getCharacterHtml()
