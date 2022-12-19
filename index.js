ded2cdb677134494cf78a4dbb9bff5e2666f8d60
ranjitsgill <82792839+ranjitsinghgill@users.noreply.github.com>
2022-12-19 21:46:03 +1100

    created a new file utils.js

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
-import characterData from './data.js'
+import characterData from '/data.js'
+import getDiceRollArray from '/utils.js'
 
-function getDiceRollArray(diceCount) {  
-    return new Array(diceCount).fill(0).map(function(){
-    return Math.floor(Math.random() * 6) + 1
-    });   
-}
-
-
+/*15*/
 
 function Character(data) {
     Object.assign(this, data)
